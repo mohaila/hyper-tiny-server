45f48b5569d810e9ab75efb4e4b1fa33be07d3b8
mohaila <mohaila@xubudesk27>
2019-09-21 14:35:18 -0400

    Add handler function for / and 404 response otherwise

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use hyper::{Body, Response, Server};
+use hyper::service::service_fn;
+use hyper::{Body, Response, Server, Request, Error, Method, StatusCode};
+use futures::{future, Future};
 
 fn main() {
-    let addr = ([127, 0, 0, 1], 8080).into();
+    let addr = ([0, 0, 0, 0], 8080).into();
     let builder = Server::bind(&addr);
-    let server =
-        builder.serve(|| service_fn_ok(|_| Response::new(Body::from("Hello from Rust Server"))));
+    let server = builder.serve(|| service_fn(handler));
     let server = server.map_err(drop);
     hyper::rt::run(server);
+}
+
+fn handler(req: Request<Body>) -> impl Future<Item=Response<Body>, Error=Error> {
+    match(req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            future::ok(Response::new("Hello from Rust!".into()))
+        },
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+            future::ok(response)
+        }
+    }
 }
